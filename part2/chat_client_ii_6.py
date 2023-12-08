913d4a80f18bf1c94e7612961e4189882b63acc5
ahliko <killian.guillemot@ynov.com>
2023-12-08 11:39:49 +0100

    Update part2

---
+++ part2/chat_client_ii_6.py
@@ -0,0 +1,73 @@
+import asyncio
+import aioconsole
+import argparse
+
+
+class Client:
+    def __init__(self, host="127.0.0.1", port=8888):
+        self.__host = host
+        self.__port = port
+        self.__pseudo = None
+        self.__reader = None
+        self.__writer = None
+        asyncio.run(self.run())
+
+    async def __async_input(self):
+        while True:
+            try:
+                input_coro = await aioconsole.ainput("Enter your message: ")
+                self.__writer.write(input_coro.encode())
+                await self.__writer.drain()
+            except KeyboardInterrupt:
+                print("Bye!")
+                self.__writer.write("".encode())
+                self.__writer.close()
+                await self.__writer.wait_closed()
+                exit(0)
+
+    async def __async_receive(self):
+        while True:
+            data = await self.__reader.read(1024)
+            if data == b'':
+                return
+            message = data.decode()
+            print(f"Received {message!r}")
+
+    async def __async_pseudo(self):
+        input_coro = await aioconsole.ainput("Enter your pseudo: ")
+        if input_coro == "":
+            print("Pseudo cannot be empty.")
+            return False
+        self.__pseudo = input_coro
+        self.__writer.write(f"Hello|{self.__pseudo}".encode())
+        await self.__writer.drain()
+        return True
+
+    async def run(self):
+        try:
+            self.__reader, self.__writer = await asyncio.open_connection(host=self.__host, port=self.__port)
+            if await self.__async_pseudo():
+                while True:
+                    await asyncio.gather(*[self.__async_input(),
+                                           self.__async_receive()])
+        except KeyboardInterrupt:
+            print("Bye!")
+            self.__writer.close()
+            await self.__writer.wait_closed()
+            exit(0)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description="Client de chat.")
+    argparser.add_argument("-p", "--port", type=int, help="Port de connexion du serveur")
+    argparser.add_argument("-a", "--address", type=str, help="Adresse de connexion du serveur")
+    argv = argparser.parse_args()
+    if argv.port:
+        if argv.address:
+            Client(host=argv.address, port=argv.port)
+        else:
+            Client(port=argv.port)
+    elif argv.address:
+        Client(host=argv.address)
+    else:
+        Client()
---
+++ part2/chat_server_ii_6.py
@@ -0,0 +1,97 @@
+import argparse
+import asyncio
+
+
+class Server:
+    def __init__(self, host="127.0.0.1", port=8888):
+        self.__host = host
+        self.__port = port
+        self.__clients = {}
+        asyncio.run(self.run())
+
+    async def __handle_client_msg(self, reader, writer):
+        print(f"New client : {writer.get_extra_info('peername')}")
+        if writer.get_extra_info('peername') not in self.__clients:
+            data = await reader.read(1024)
+            if data == b'':
+                writer.write("You must choose un nametag".encode())
+                writer.close()
+                return
+            if data.decode().startswith("Hello|"):
+                self.__clients[writer.get_extra_info('peername')] = {}
+                self.__clients[writer.get_extra_info('peername')]["r"] = reader
+                self.__clients[writer.get_extra_info('peername')]["w"] = writer
+                self.__clients[writer.get_extra_info('peername')]["here"] = True
+                self.__clients[writer.get_extra_info('peername')]['pseudo'] = data.decode()[6:]
+                await self.__send_all("", writer.get_extra_info('peername'), True)
+            else:
+                writer.write("You must choose un nametag".encode())
+                writer.close()
+                return
+        else:
+            self.__clients[writer.get_extra_info('peername')]["r"] = reader
+            self.__clients[writer.get_extra_info('peername')]["w"] = writer
+            self.__clients[writer.get_extra_info('peername')]["here"] = True
+        while True:
+            data = await \
+                self.__clients[writer.get_extra_info('peername')][
+                    "r"].read(
+                    1024)
+            client = writer.get_extra_info('peername')
+            if data == b'':
+                self.__clients[client]["here"] = False
+                self.__clients[client]["w"].close()
+                self.__clients[client]["w"] = None
+                await self.__send_all("", client, disconnect=True)
+                break
+            message = data.decode()
+            print(
+                f"Message received from {self.__clients[client]['w'].get_extra_info('peername')[0]}:{self.__clients[client]['w'].get_extra_info('peername')[1]} : {message!r}")
+            await self.__send_all(message, client)
+
+    async def __send_all(self, message, localclient, annonce=False, disconnect=False):
+        for client in self.__clients:
+            if self.__clients[client]["here"]:
+                if not annonce:
+                    if client != localclient:
+                        if disconnect:
+                            print(f"Annonce : {self.__clients[localclient]['pseudo']} a quitté la chatroom")
+                            self.__clients[client]["w"].write(
+                                f"Annonce : {self.__clients[localclient]['pseudo']} a quitté la chatroom".encode())
+                            await self.__clients[client]["w"].drain()
+                        else:
+                            self.__clients[client]["w"].write(
+                                f"{self.__clients[localclient]['pseudo']} a dit : {message}".encode())
+                            await self.__clients[client]["w"].drain()
+                else:
+                    print(f"Annonce : {self.__clients[localclient]['pseudo']} a rejoint la chatroom")
+
+                    self.__clients[client]["w"].write(
+                        f"Annonce : {self.__clients[localclient]['pseudo']} a rejoint la chatroom".encode())
+                    print(client)
+                    await self.__clients[client]["w"].drain()
+        if disconnect:
+            self.__clients.pop(localclient)
+
+    async def run(self):
+        server = await asyncio.start_server(self.__handle_client_msg, self.__host, self.__port)
+        addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+        print(f'Serving on {addrs}')
+        async with server:
+            await server.serve_forever()
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description="Client de chat.")
+    argparser.add_argument("-p", "--port", type=int, help="Port de connexion du serveur")
+    argparser.add_argument("-a", "--address", type=str, help="Adresse de connexion du serveur")
+    argv = argparser.parse_args()
+    if argv.port:
+        if argv.address:
+            Server(host=argv.address, port=argv.port)
+        else:
+            Server(port=argv.port)
+    elif argv.address:
+        Server(host=argv.address)
+    else:
+        Server()
